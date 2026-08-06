27508beddcc072ba935a3f7de7e61509e3f57520
Kauê <kauenonato84@gmail.com>
2026-08-05 21:10:14 -0300

    Atualizar o simulação.php

--- simulação.php
+++ simulação.php
@@ -5,15 +5,16 @@ $idade = $_POST["num_idade"];
 $valor = $_POST["num_valor";
 
  if ($parcelas == 1)
-    {
-        echo "<h1> Você pagará R$ $valor </h1>";
-    }
-     else if ($parcelas == 12)
-     {
-     $juros = $parcelas + 0.05;
-     echo "<h1> Você pagará R$ $valor </h1>"
-     }
-     else
         {
-            
+        echo "<h1> Você pagará R$ $valor </h1>";
+        }
+     else if ($parcelas == 12)
+         {
+         $juros = $parcelas + 0.05;
+         echo "<h1> Você pagará R$ $valor </h1>"
+         }
+     else if ($parcelas == 24)
+        {
+        $juros = $parcelas + 0.12;
+        echo "<h1> Você pagará R$ $valor </h1>"
         }
