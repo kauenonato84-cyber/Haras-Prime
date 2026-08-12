cdc25dbdab872509677c265c69a1b7ce438ce25c
MicaPitombo <mshow27005@gmail.com>
2026-08-11 22:30:28 -0300

    Corrigir lógica condicional e cálculos em simulação.php

--- simulação.php
+++ simulação.php
@@ -4,22 +4,25 @@ $parcelas = $_POST["sel_parcelas"];
 $idade = $_POST["num_idade"];
 $valor = $_POST["num_valor";
 
- if ($parcelas == 1)
-        {
-        echo "<h1> Você pagará R$ $valor </h1>";
+$valor_final = $valor;
+
+ if ($parcelas == 1){
+         $valor_final = $valor
         }
-     else if ($parcelas == 12)
-         {
-         $juros = $parcelas + 0.05;
-         echo "<h1> Você pagará R$ $valor </h1>"
+     else if ($parcelas == 12){
+         $valor_final = $valor * 1.05;
          }
-     else if ($parcelas == 24)
-        {
-        $juros = $parcelas + 0.12;
-        echo "<h1> Você pagará R$ $valor </h1>"
+     else if ($parcelas == 24){
+        $valor_final = $valor * 1.12;
         }
-     else if ($idade >= 18 && $valor >= 30000)
-     {
-     
+
+     $valor_final = $valor / $parcelas;
+
+     echo "<h1>Valor total: R$ $valor_final</h1>";
+     echo "<h3>Valor de cada parcela ($parcelas x): R$ $valor_parcela</h3>";
+
+     else if ($idade >= 18 && $valor >= 30000){
+          echo "<h2>Como cliente especial, você foi contemplado com um Cupom VIP no valor de R$ 1.000,00 para sua próxima aquisição</h2>";
+     }
       
       
