fcb34ff4e265b6972dab5c51187ffff318eaf663
Kauê <kauenonato84@gmail.com>
2026-08-12 18:34:25 -0300

    Outra atualização do simulação.php, agora vai 🙏

--- simulação.php
+++ simulação.php
@@ -6,23 +6,23 @@ $valor = $_POST["num_valor"];
 
 $valor_final = $valor;
 
- if ($parcelas == 1){
-         $valor_final = $valor;
-        }
-     else if ($parcelas == 12){
-         $valor_final = $valor * 1.05;
-         }
-     else if ($parcelas == 24){
-        $valor_final = $valor * 1.12;
-        }
+if ($parcelas == 1) {
+    $valor_final = $valor;
+} 
+else if ($parcelas == 12) {
+    $valor_final = $valor * 1.05;
+} 
+else if ($parcelas == 24) {
+    $valor_final = $valor * 1.12;
+}
 
-     $valor_final = $valor / $parcelas;
+$valor_parcela = $valor_final / $parcelas;
 
-     echo "<h1>Valor total: R$ $valor_final</h1>";
-     echo "<h3>Valor de cada parcela ($parcelas x): R$ $valor_parcela</h3>";
+echo "<h1>Valor total: R$ " . number_format($valor_final, 2, ',', '.') . "</h1>";
+echo "<h3>Valor de cada parcela ($parcelas x): R$ " . number_format($valor_parcela, 2, ',', '.') . "</h3>";
 
-     else if ($idade >= 18 && $valor >= 30000){
-          echo "<h2>Como cliente especial, você foi contemplado com um Cupom VIP no valor de R$ 1.000,00 para sua próxima aquisição</h2>";
-     }
-      
+if ($idade >= 18 && $valor >= 30000) {
+    echo "<h2>Como cliente especial, você foi contemplado com um Cupom VIP no valor de R$ 1.000,00 para sua próxima aquisição</h2>";
+}
+
       
